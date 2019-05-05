177759c56b06c120d709904e581a8a0f47d970d1
Jamo <jamofer@users.noreply.github.com>
2019-05-05 17:03:49 +0200

    updated project web mockup with procedural test_plan list

--- www/lib/project.js
+++ www/lib/project.js
@@ -9,7 +9,7 @@ const ROLE = {
     tester: 'Tester',
 };
 
-function project_string_avatar(string) {
+function string_avatar(string) {
     const words = string.split(" ").slice(0, 2);
     return words.map((word) => {
         return word[0];
@@ -18,7 +18,7 @@ function project_string_avatar(string) {
 
 const project_list_element_template = (project) => `
 <li class="collection-item avatar">
-    <i class="circle">${project_string_avatar(project.title)}</i>
+    <i class="circle">${string_avatar(project.title)}</i>
     <a class="title black-text" href="${project.web_url}">${project.title}</a>
     <i class="material-icons tiny">${VISIBILITY_ICON[project.visibility]}</i>
     <span class="role new badge" data-badge-caption="${ROLE[project.permissions.role]}"></span>
@@ -26,7 +26,24 @@ const project_list_element_template = (project) => `
 </li>
 `;
 
+const test_plan_list_element_template = (test_plan) => `
+<li class="collection-item avatar">
+    <i class="circle">${string_avatar(test_plan.title)}</i>
+    <a class="title black-text" href="#">${test_plan.title}</a>
+    <p>Current build: ${test_plan.current_build.title}</p>
+    <span class="role new badge" data-badge-caption="Passed">${test_plan.current_build.passed}</span>
+    <span class="role new badge" data-badge-caption="Failed">${test_plan.current_build.failed}</span>
+    <span class="role new badge" data-badge-caption="Not run">${test_plan.current_build.not_run}</span>
+    <span class="badge" data-badge-caption="">Last update: ${test_plan.last_update}</span>
+</li>
+`;
+
+
 
 function add_project_list_element(project, container_id) {
     $(`#${container_id}`).append(project_list_element_template(project));
+}
+
+function add_test_plan_list_element(test_plan, container_id) {
+    $(`#${container_id}`).append(test_plan_list_element_template(test_plan));
 }
